d428e5c9993ce6345ae5334f9ac512e89d39c80c
Wilson Omondi <wangaw94@gmail.com>
2022-04-01 11:47:07 +0300

    Updating the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import { useState } from 'react';
-import './App.css';
+// import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+
 
 function App() {
+
+  // const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -27,6 +30,15 @@ function App() {
     }
   ])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
+
   // Deleting Tasks
   const deleteTask = (id) => {
     setTasks(tasks.filter((task)=> task.id !== id))
@@ -41,6 +53,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:'No Task To Show'}
     </div>
   );
